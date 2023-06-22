fda3b94b20ba1c27abd9978dae483a0681bd2f4b
Tonnie <tkymeu@gmail.com>
2023-06-22 17:24:17 +0300

    made changes

---
+++ src/pages/Course.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Course = () => {
+    return <div className='dark:text-white'>Profile</div>
+}
+
+export default Course
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => {
+    return <div className='dark:text-white'>Profile</div>
+}
+
+export default Login
